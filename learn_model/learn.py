e754c8980d662a8bb9335f1e1d32442be8bf1c70
SMX <142971728+SMXlol@users.noreply.github.com>
2025-04-06 20:07:07 +0700

    model learn

--- learn_model/learn.py
+++ learn_model/learn.py
@@ -1,4 +1,12 @@
-from ultralytics import YOLO
+if __name__ == "__main__":
+    from ultralytics import YOLO
 
-model = YOLO("yolov8n.pt")
-model.train(data="config.yaml", epochs=50, imgsz=640, batch=16)
+    model = YOLO("yolo11n.pt")  # load an official model
+    model.train(data="config.yaml", epochs=500, batch=64, optimizer="adam")
+    model.export(format="onnx")
+
+    metrics = model.val()  # Запускаем валидацию
+    print(f"Точность модели (mAP50-95): {metrics.box.map:.4f}")
+    print(f"Точность на IoU=0.5 (mAP50): {metrics.box.map50:.4f}")
+    print(f"Точн"
+          f"ость на IoU=0.75 (mAP75): {metrics.box.map75:.4f}")
--- learn_model/test.py
+++ learn_model/test.py
@@ -1,7 +1,13 @@
-from ultralytics import YOLO
+if __name__ == "__main__":
+      from ultralytics import YOLO
 
-model = YOLO("runs/detect/train/weights/best.pt")
+      model = YOLO("../runs/detect/train4/weights/best.pt")
 
-results = model.predict(source="yolo_dataset/images/test", save=True)
+      metrics = model.val()  # Запускаем валидацию
+      print(f"Точность модели (mAP50-95): {metrics.box.map:.4f}")
+      print(f"Точность на IoU=0.5 (mAP50): {metrics.box.map50:.4f}")
+      print(f"Точн"
+            f"ость на IoU=0.75 (mAP75): {metrics.box.map75:.4f}")
+      results = model.predict(source="yolo_dataset/images/test", save=True)
 
-print("✅ Предсказания сохранены в папке runs/detect/predict/")
+      print("✅ Предсказания сохранены в папке runs/detect/predict/")
